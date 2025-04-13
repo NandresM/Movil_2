427df1ca9428605265af8f1e7849a71ebc29f119
NandresM <emperador1105@gmail.com>
2025-04-13 11:34:37 -0500

    Version 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.camera.view)
+    implementation(libs.camera.lifecycle)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
